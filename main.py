3bea0516e9a9e75292df10c80356bb7ea825be64
ShiverMeTimbers <michelle.nyamai@gmail.com>
2017-03-18 14:33:21 +0000

    added http client and imported os, re. Also added if

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
+
+import os
+import re
+import http.client
 from flask import Flask, render_template #renders templates - HTML from within python
 app = Flask(__name__)
 
-
-
-@app.route("/") #tells decorator to tell Flsk what URL should trigger the function
-@app.route("/<user>") 
-def index(user=None): 
-	return render_template("user.html", user=user)
 
 @app.route("/shopping") #tells decorator to tell Flsk what URL should trigger the function
 def shopping():
 	food = ["Cheese", "Tuna", "Beef"]
 	return render_template("shopping.html", food=food)
+
+conn = http.client.HTTPSConnection("api.sportradar.us")
+
+conn.request("GET", "/nba-t3/seasontd/2013/REG/rankings.xml?api_key={kut24tmdqv3uwm6u4gbauan2}")
+
+res = conn.getresponse()
+data = res.read()
+
+print(data.decode("utf-8"))
+
+if __name__ == '__main__':
+    if 'PORT' in os.environ:
+        app.run(host='0.0.0.0', port=int(os.environ['PORT']))
+    else:
+        app.run(debug=True, port=80)
+
 
 
