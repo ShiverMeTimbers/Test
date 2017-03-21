f2f72eb11e049f1b46854be7795e8c0670f09da3
ShiverMeTimbers <michelle.nyamai@gmail.com>
2017-03-21 23:50:41 +0000

    Updated library & added keys.py

---
+++ keys.py
@@ -0,0 +1,8 @@
+# this contains important info to send emails
+
+# adding details of your account
+consumer_key = '1i8X7aK9wEDiMNyT6CU0flXOb' 
+consumer_secret = 't380PIBsH7HaDXZGrMIIJzIoxfxwt6kwLZkBoKDdAVbfureajH'
+
+access_token = '3676320621-Wz4jJMATiGHtTWKlrGJD6ysdIrfdp8LIaOP488B'
+access_level_secret =  's8x0DsBi4aD5yCoTmATmqsoMLcQoMcdJTypC9UKqS2p9y'
