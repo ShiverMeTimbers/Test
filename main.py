18548fde9f75ac7b6c943e0c4f1c8375be133a05
ShiverMeTimbers <michelle.nyamai@gmail.com>
2017-03-21 13:26:19 +0000

    modified app route

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 
-import requests
-
 from flask import Flask, render_template #renders templates - HTML from within python
 app = Flask(__name__)
 
 
-@app.route('/shopping') #tells decorator to tell Flsk what URL should trigger the function
+@app.route('/') #tells decorator to tell Flsk what URL should trigger the function
 def shopping():
 	food = ["Cheese", "Tuna", "Beef"]
 	return render_template("shopping.html", food=food)
