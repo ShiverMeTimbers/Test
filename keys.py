0d3401b6b13e724b41113cc5289e846091f747f4
ShiverMeTimbers <michelle.nyamai@gmail.com>
2017-03-22 08:51:36 +0000

    updated keys and main for tweepy

--- keys.py
+++ keys.py
@@ -5,4 +5,4 @@ consumer_key = '1i8X7aK9wEDiMNyT6CU0flXOb'
 consumer_secret = 't380PIBsH7HaDXZGrMIIJzIoxfxwt6kwLZkBoKDdAVbfureajH'
 
 access_token = '3676320621-Wz4jJMATiGHtTWKlrGJD6ysdIrfdp8LIaOP488B'
-access_level_secret =  's8x0DsBi4aD5yCoTmATmqsoMLcQoMcdJTypC9UKqS2p9y'
+access_token_secret = 's8x0DsBi4aD5yCoTmATmqsoMLcQoMcdJTypC9UKqS2p9y'
--- main.py
+++ main.py
@@ -1,24 +1,25 @@
 import tweepy
 import os
+from flask import Flask
+from tweepy import OAuthHandler
 from keys import *
 
-auth = tweepy.OAuthHandler('consumer_key', 'consumer_secret')
-auth.set_access_token('access_token', 'access_level_secret')
+app = Flask(__name__)
 
-twitter_api = tweepy.API('auth')
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
 
-cfg_tweets = twitter_api.search(
-	q="CodeFirstGirls"
-)
+api = tweepy.API(auth)
 
-for tweet in cfg_tweets:
-	print tweet.user.name + ": " + tweet.text
+public_tweets = api.home_timeline()
+for tweet in public_tweets:
+    print tweet.text
 
-if 'PORT' in os.environ:
-     app.run(host='0.0.0.0', port=int(os.environ['PORT']))
-else:
-     app.run(debug=True)
-
+if __name__ == '__main__':
+    if 'PORT' in os.environ:
+        app.run(host='0.0.0.0', port=int(os.environ['PORT']))
+    else:
+        app.run(debug=True, port=80)
 
 
 
